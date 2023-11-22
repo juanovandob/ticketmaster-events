7bc13325321344af1a9edbaaa48da3d383936c31
Juan Carlos <juanovandob@gmail.com>
2023-11-22 13:21:05 -0600

    feat: Add search input get value from

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,17 +2,33 @@
 //es una función flecha. y un export
 /**
  * const Navbar = () => {};
- * 
+ *
  * export default Navbar;
  */
+import { useState } from "react";
+
 //First component
 const Navbar = () => {
-    return (
-        <div>
-            <p>Mi Boletera</p>
-            <input placeholder="Busca tu evento favorito"/>
-        </div>
-    );
+  //search = valor del input (puede tener cualquier nombre), setSearch = función que actualiza el valor del input
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = (evt) => {
+    //setSearch mantiene actualizado el valor del input
+    setSearch(evt.target.value);
+  };
+
+  console.log(search);
+  return (
+    <div>
+      <p>Mi Boletera</p>
+      <input
+        placeholder="Busca tu evento favorito"
+        //acá se define el evento que llama a la funcion
+        onChange={handleInputChange}
+        value={search}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
